bf2fad94a1a4c58dbb27944364f5a247e8c56c89
Omni <omni@numbersprotocol.io>
2026-04-16 09:28:09 +0000

    feat: update src/app/features/home/collection-tab/collection-tab/coll...
    by Olga Shen & Omni

--- src/app/features/settings/settings.page.spec.ts
+++ src/app/features/settings/settings.page.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { CaptureAppWebCryptoApiSignatureProvider } from '../../shared/collector/signature/capture-app-web-crypto-api-signature-provider/capture-app-web-crypto-api-signature-provider.service';
+import { DiaBackendAuthService } from '../../shared/dia-backend/auth/dia-backend-auth.service';
 import { SharedTestingModule } from '../../shared/shared-testing.module';
 import { SettingsPage } from './settings.page';
 
@@ -6,10 +9,30 @@ describe('SettingsPage', () => {
   let component: SettingsPage;
   let fixture: ComponentFixture<SettingsPage>;
 
+  const diaBackendAuthServiceStub = {
+    email$: of('tester@example.com'),
+    emailVerified$: of(false),
+    syncUser$: jasmine.createSpy().and.returnValue(of(void 0)),
+  };
+
+  const signatureProviderStub = {
+    privateKey$: of('private-key-for-test'),
+  };
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [SettingsPage],
       imports: [SharedTestingModule],
+      providers: [
+        {
+          provide: DiaBackendAuthService,
+          useValue: diaBackendAuthServiceStub,
+        },
+        {
+          provide: CaptureAppWebCryptoApiSignatureProvider,
+          useValue: signatureProviderStub,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SettingsPage);
