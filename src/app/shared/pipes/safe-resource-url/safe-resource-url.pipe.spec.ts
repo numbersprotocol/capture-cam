641f132448f7f483c78b0a634d8dcdc286bb6a44
Omni <omni@numbersprotocol.io>
2026-04-16 02:50:52 +0000

    fix: validate trusted URLs in SafeResourceUrlPipe
    by Olga Shen & Omni

--- src/app/shared/pipes/safe-resource-url/safe-resource-url.pipe.spec.ts
+++ src/app/shared/pipes/safe-resource-url/safe-resource-url.pipe.spec.ts
@@ -1,4 +1,5 @@
 import { DomSanitizer } from '@angular/platform-browser';
+import { BUBBLE_IFRAME_URL } from '../../dia-backend/secret';
 import { SafeResourceUrlPipe } from './safe-resource-url.pipe';
 
 describe('SafeResourceUrlPipe', () => {
@@ -22,5 +23,24 @@ describe('SafeResourceUrlPipe', () => {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const pipe = new SafeResourceUrlPipe(domSanitizerMock!);
     expect(pipe).toBeTruthy();
+  });
+
+  it('should bypass security for a trusted URL prefixed with BUBBLE_IFRAME_URL', () => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const pipe = new SafeResourceUrlPipe(domSanitizerMock!);
+    const trustedUrl = `${BUBBLE_IFRAME_URL}/wallet`;
+    expect(pipe.transform(trustedUrl)).toBe(trustedUrl);
+  });
+
+  it('should block an untrusted URL and return an empty safe resource URL', () => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const pipe = new SafeResourceUrlPipe(domSanitizerMock!);
+    expect(pipe.transform('javascript:alert(1)')).toBe('');
+  });
+
+  it('should block an empty URL and return an empty safe resource URL', () => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const pipe = new SafeResourceUrlPipe(domSanitizerMock!);
+    expect(pipe.transform('')).toBe('');
   });
 });
