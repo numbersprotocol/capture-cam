baa101b0fda586fa87b3f11d51ce9b82d5bdffee
Omni <omni@numbersprotocol.io>
2026-08-06 09:18:44 +0000

    fix: initialize Ionic standalone runtime in app module
    by Olga Shen & Omni

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { APP_INITIALIZER, NgModule, NgZone } from '@angular/core';
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { initialize } from '@ionic/core/components';
+import type { IonicConfig } from '@ionic/core/components';
 import { FormlyModule } from '@ngx-formly/core';
 import { FormlyMaterialModule } from '@ngx-formly/material';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +15,16 @@ import { HammerModule } from './shared/hammer/hammer.module';
 import { TranslocoRootModule } from './shared/language/transloco/transloco-root.module';
 import { SharedModule } from './shared/shared.module';
 import { provideUserGuideTour } from './shared/user-guide/user-guide.module';
+
+function initializeIonicStandaloneRuntime(doc: Document, zone: NgZone) {
+  return () => {
+    doc.documentElement.classList.add('ion-ce');
+    initialize({
+      mode: 'md',
+      _zoneGate: (handler: () => unknown) => zone.run(handler),
+    } as IonicConfig);
+  };
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -28,6 +41,12 @@ import { provideUserGuideTour } from './shared/user-guide/user-guide.module';
   ],
   providers: [
     provideUserGuideTour(),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeIonicStandaloneRuntime,
+      multi: true,
+      deps: [DOCUMENT, NgZone],
+    },
     {
       provide: RouteReuseStrategy,
       useClass: IonicRouteStrategy,
